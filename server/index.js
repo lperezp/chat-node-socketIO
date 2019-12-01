5474bdc75b1932af61eb183c7318e56df75892cf
lperezp <luiseperezp.10@gmail.com>
2019-12-01 18:22:09 -0500

    feat(client): add page index with Bootstrap v.4.3

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
+// emitir un sms a un cliente de un socket
+app.get('/index',(req,res)=>{
+    res.status(200).send('Hola mundo desde una ruta');
+})
+
 // cargar el modulo de socketIO
 var io = require('socket.io')(server);
 
@@ -10,3 +15,12 @@ var io = require('socket.io')(server);
 server.listen(2019,()=>{
     console.log('Servidor esta funcionando en http://localhost:2019');
 });
+
+// lanzamiento de eventos de socketIO
+io.on('connection',(socket)=>{
+    console.log("Hay un cliente nuevo", socket)
+})
+
+// cargar una vista estatica 
+
+app.use(express.static('client'))
